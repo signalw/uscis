01a4d884926f58f60aa91a73e49b1e5589e12a6e
signalw <xinhao@brandeis.edu>
2017-06-02 21:26:18 -0400

    sort log

---
+++ sort_log.py
@@ -0,0 +1,20 @@
+"""
+This script sorts the output of check_status,
+assume the output is stored in a file
+
+Usage:
+    $ python sort_log.py output1.log output2.log
+"""
+
+import sys
+
+def sort_log(f_name):
+    with open(f_name, "r+") as f:
+        cases = sorted([c.strip().split(' ', 1) for c in f.readlines()])
+        f.seek(0)
+        f.write('\n'.join([' '.join(c) for c in cases]))
+        f.truncate()
+
+if __name__ == '__main__':
+    for f in sys.argv[1:]:
+        sort_log(f)
